4e543a4bb89ac3776286a4af165c974528bd4523
y-anne <yean@umich.edu>
2020-09-19 19:52:47 -0400

    New changes

--- my_app/views.py
+++ my_app/views.py
@@ -6,3 +6,12 @@ import requests
 def index():
     return render_template("index.html")
 
+@app.route("/about_us/")
+def about_us_page():
+    return render_template("about_us.html")
+
+@app.route("/create_event/")
+def create_event_page():
+    return render_template("create_event.html")
+
+
