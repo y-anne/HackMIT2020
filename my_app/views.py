22ddc80099fc65e7fc1db79b0aea5f56b132d1d5
y-anne <yean@umich.edu>
2020-09-19 21:59:23 -0400

    Added form function

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,5 @@
 from my_app import app
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, Flask, jsonify, json
 import requests
 
 @app.route("/")
@@ -14,4 +14,19 @@ def about_us_page():
 def create_event_page():
     return render_template("create_event.html")
 
+@app.route('/add_challenge', methods = ['POST']) 
+def add_challenge(): 
+   duration_out = request.form['duration']
+   emails_out = request.form['emails']
+   description_out = request.form['description']
+   data = {'duration' : duration_out,
+            'emails' : emails_out,
+            'description': description_out
+            }
+    return jsonify(data)
+
+   #print(duration_out)
+   #print(emails_out)
+   #print(description_out)
+   #return redirect("/")
 
